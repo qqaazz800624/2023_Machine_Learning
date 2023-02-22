73c104c350d0d649e819ce58051de4232dff26d9
qqaazz800624 <qqaazz800624@gmail.com>
2023-02-22 22:54:33 +0800

    import tqdm

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -9,13 +9,7 @@ import os
 import csv
 
 # For Progress Bar
-from tqdm import tqdm
+import tqdm
 #%%
-# Pytorch
-import torch 
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, random_split
 
-# For plotting learning curve
-from torch.utils.tensorboard import SummaryWriter
 #%%
