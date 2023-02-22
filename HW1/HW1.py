fb832e4db6884084bf5c1cab45fc22bc2c81a88a
qqaazz800624 <qqaazz800624@gmail.com>
2023-02-22 22:57:32 +0800

    define functions

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -9,7 +9,43 @@ import os
 import csv
 
 # For Progress Bar
-import tqdm
+from tqdm import tqdm
+
+# Pytorch
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+
+# For plotting learning curve
+from torch.utils.tensorboard import SummaryWriter
 #%%
+
+def same_seed(seed): 
+    '''Fixes random number generator seeds for reproducibility.'''
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+def train_valid_split(data_set, valid_ratio, seed):
+    '''Split provided training data into training set and validation set'''
+    valid_set_size = int(valid_ratio * len(data_set)) 
+    train_set_size = len(data_set) - valid_set_size
+    train_set, valid_set = random_split(data_set, [train_set_size, valid_set_size], generator=torch.Generator().manual_seed(seed))
+    return np.array(train_set), np.array(valid_set)
+
+def predict(test_loader, model, device):
+    model.eval() # Set your model to evaluation mode.
+    preds = []
+    for x in tqdm(test_loader):
+        x = x.to(device)                        
+        with torch.no_grad():                   
+            pred = model(x)                     
+            preds.append(pred.detach().cpu())   
+    preds = torch.cat(preds, dim=0).numpy()  
+    return preds
+
 
 #%%
