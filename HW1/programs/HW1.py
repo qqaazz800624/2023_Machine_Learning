ccb5af77933314480b25642eeb3b278384b6f1a1
qqaazz800624 <qqaazz800624@gmail.com>
2023-02-24 16:29:29 +0800

    first try on 20230224

--- HW1/programs/HW1.py
+++ HW1/programs/HW1.py
@@ -23,6 +23,9 @@ from torch.utils.tensorboard import SummaryWriter
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn import preprocessing
 
+from IPython.display import FileLink
+
+
 def same_seed(seed): 
     '''Fixes random number generator seeds for reproducibility.'''
     torch.backends.cudnn.deterministic = True
@@ -93,29 +96,182 @@ class My_Model(nn.Module):
         x = x.squeeze(1) # (B, 1) -> (B)
         return x
 
+
+def select_feat(train_data, valid_data, test_data, select_all=True):
+    '''Selects useful features to perform regression'''
+    y_train, y_valid = train_data[:,-1], valid_data[:,-1]
+    raw_x_train, raw_x_valid, raw_x_test = train_data[:,:-1], valid_data[:,:-1], test_data
+
+    if select_all:
+        feat_idx = list(range(raw_x_train.shape[1]))
+    else:
+        # feat_idx = [0,1,2,3,4] # TODO: Select suitable feature columns.
+        
+        # select by anova
+        select_k = 30
+        fs = SelectKBest(score_func=f_regression, k=select_k)
+        results = fs.fit(raw_x_train, y_train)
+        feat_idx = fs.get_support(indices=True)
+        setA = set([i for i in feat_idx])
+        
+        # I think the following is important, so I choose artificial
+        zone = [i for i in range(1, 38)] # zone
+        pos4 = [53, 69, 85, 101] # positive rate of four day
+        lastday = [i for i in range(102, 117)] # the last day information
+        arti = zone+pos4+lastday
+        setB = set(arti)
+
+        # choose union
+        setC = setB.union(setA)
+        feat_idx = list(setC)
+        print(feat_idx)
+ 
+    return raw_x_train[:,feat_idx], raw_x_valid[:,feat_idx], raw_x_test[:,feat_idx], y_train, y_valid
+
+
+
+def trainer(train_loader, valid_loader, model, config, device):
+
+    criterion = nn.MSELoss(reduction='mean') # Define your loss function, do not modify this.
+
+    # Define your optimization algorithm. 
+    # TODO: Please check https://pytorch.org/docs/stable/optim.html to get more available algorithms.
+    # TODO: L2 regularization (optimizer(weight decay...) or implement by your self).
+    # optimizer = torch.optim.SGD(model.parameters(), lr=config['learning_rate'], momentum=0.7) 
+    # change the optimizer from SGD to Adam
+    optimizer = torch.optim.Adam(model.parameters()
+                                ,lr=config['learning_rate']
+                                ,betas=(0.9, 0.999)
+                                ,eps=1e-08
+                                ,weight_decay=config['w_decay_rate']
+                                 )
+    writer = SummaryWriter() # Writer of tensoboard.
+
+    if not os.path.isdir('./models'):
+        os.mkdir('./models') # Create directory of saving models.
+
+    n_epochs, best_loss, step, early_stop_count = config['n_epochs'], math.inf, 0, 0
+
+    for epoch in range(n_epochs):
+        # Training Part
+        model.train() # Set your model to train mode.
+        loss_record = []
+
+        # tqdm is a package to visualize your training progress.
+        train_pbar = tqdm(train_loader, position=0, leave=True)
+
+        for x, y in train_pbar:
+            optimizer.zero_grad()               # Set gradient to zero.
+            x, y = x.to(device), y.to(device)   # Move your data to device. 
+            pred = model(x)             
+            loss = criterion(pred, y)
+            loss.backward()                     # Compute gradient(backpropagation).
+            optimizer.step()                    # Update parameters.
+            step += 1
+            loss_record.append(loss.detach().item())
+            
+            # Display current epoch number and loss on tqdm progress bar.
+            train_pbar.set_description(f'Epoch [{epoch+1}/{n_epochs}]')
+            train_pbar.set_postfix({'loss': loss.detach().item()})
+
+        mean_train_loss = sum(loss_record)/len(loss_record)
+        writer.add_scalar('Loss/train', mean_train_loss, step)
+
+        # Validation / Evaluation Part
+        model.eval() # Set your model to evaluation mode.
+        loss_record = []
+        for x, y in valid_loader:
+            x, y = x.to(device), y.to(device)
+            with torch.no_grad():
+                pred = model(x)
+                loss = criterion(pred, y)
+
+            loss_record.append(loss.item())
+            
+        mean_valid_loss = sum(loss_record)/len(loss_record)
+        print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
+        # writer.add_scalar('Loss/valid', mean_valid_loss, step)
+
+        if mean_valid_loss < best_loss:
+            best_loss = mean_valid_loss
+            torch.save(model.state_dict(), config['save_path']) # Save your best model
+            print('Saving model with loss {:.3f}...'.format(best_loss))
+            early_stop_count = 0
+        else: 
+            early_stop_count += 1
+
+        if early_stop_count >= config['early_stop']:
+            print('\nModel is not improving, so we halt the training session.')
+            return
+
 #%%
 
-
-
-#%%
-
-
-
-
-
-
-#%%
-
-
-
-#%%
-
-
-
-
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+config = {
+    'seed': 520,      # Your seed number, you can pick your lucky number. :)
+    'select_all': True,   # Whether to use all features.
+    'valid_ratio': 0.2,   # validation_size = train_size * valid_ratio
+    'n_epochs': 5000,     # Number of epochs.            
+    'batch_size': 256, 
+    'w_decay_rate': 0.005, # weight regularization
+    'learning_rate': 1e-3,              
+    'early_stop': 500,    # If model has not improved for this many consecutive epochs, stop training.     
+    'save_path': './models/model.ckpt'  # Your model will be saved here.
+}
 
 #%%
 
+# Set seed for reproducibility
+same_seed(config['seed'])
 
+# train_data size: 3009 x 89 (35 states + 18 features x 3 days) 
+# test_data size: 997 x 88 (without last day's positive rate)
+train_data, test_data = pd.read_csv('/home/u/qqaazz800624/2023_Machine_Learning/HW1/dataset/covid_train.csv').values, pd.read_csv('/home/u/qqaazz800624/2023_Machine_Learning/HW1/dataset/covid_test.csv').values
+train_data, valid_data = train_valid_split(train_data, config['valid_ratio'], config['seed'])
+
+# Print out the data size.
+print(f"""train_data size: {train_data.shape} 
+valid_data size: {valid_data.shape} 
+test_data size: {test_data.shape}""")
+
+# Select features
+x_train, x_valid, x_test, y_train, y_valid = select_feat(train_data, valid_data, test_data, config['select_all'])
+
+# Print out the number of features.
+print(f'number of features: {x_train.shape[1]}')
+
+train_dataset, valid_dataset, test_dataset = COVID19Dataset(x_train, y_train), \
+                                            COVID19Dataset(x_valid, y_valid), \
+                                            COVID19Dataset(x_test)
+
+# Pytorch data loader loads pytorch dataset into batches.
+train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
+valid_loader = DataLoader(valid_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False, pin_memory=True)
+
+#%% Start training process
+
+model = My_Model(input_dim=x_train.shape[1]).to(device) # put your model and data on the same computation device.
+trainer(train_loader, valid_loader, model, config, device)
+
+#%%
+
+def save_pred(preds, file):
+    ''' Save predictions to specified file '''
+    with open(file, 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['id', 'tested_positive'])
+        for i, p in enumerate(preds):
+            writer.writerow([i, p])
+
+model = My_Model(input_dim=x_train.shape[1]).to(device)
+model.load_state_dict(torch.load(config['save_path']))
+preds = predict(test_loader, model, device) 
+save_pred(preds, 'pred.csv')      
+
+#%%
+
+from IPython.display import FileLink
+FileLink(r'pred.csv')
 
 #%%
