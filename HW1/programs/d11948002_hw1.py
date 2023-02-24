f16e3cda7cc2571d8d2c89f4ac8dff47c1056cbd
qqaazz800624 <qqaazz800624@gmail.com>
2023-02-24 20:31:02 +0800

    model over strong baseline

--- HW1/programs/d11948002_hw1.py
+++ HW1/programs/d11948002_hw1.py
@@ -94,51 +94,6 @@ class My_Model(nn.Module):
         x = x.squeeze(1) # (B, 1) -> (B)
         return x
 
-#%%
-'''
-
-device = 'cuda:1' if torch.cuda.is_available() else 'cuda:2'
-config = {
-    'seed': 520,      # Your seed number, you can pick your lucky number. :)
-    'select_all': True,   # Whether to use all features.
-    'valid_ratio': 0.25,   # validation_size = train_size * valid_ratio
-    'n_epochs': 5000,     # Number of epochs.            
-    'batch_size': 256, 
-    'w_decay_rate': 0.005, # weight regularization
-    'learning_rate': 3e-4,              
-    'early_stop': 500,    # If model has not improved for this many consecutive epochs, stop training.     
-    'save_path': './models/model.ckpt'  # Your model will be saved here.
-}
-
-
-train_data, test_data = pd.read_csv('/home/u/qqaazz800624/2023_Machine_Learning/HW1/dataset/covid_train.csv').values, pd.read_csv('/home/u/qqaazz800624/2023_Machine_Learning/HW1/dataset/covid_test.csv').values
-train_data, valid_data = train_valid_split(train_data, config['valid_ratio'], config['seed'])
-
-y_train, y_valid = train_data[:,-1], valid_data[:,-1]
-raw_x_train, raw_x_valid, raw_x_test = train_data[:,:-1], valid_data[:,:-1], test_data
-
-select_k = 25
-fs = SelectKBest(score_func=f_regression, k=select_k)
-results = fs.fit(raw_x_train, y_train)
-feat_idx = fs.get_support(indices=True)
-setA = set([i for i in feat_idx])
-
-states = [i for i in range(1, 34)]
-covidlike = [35, 36, 53, 54, 71, 72] # covid-like illness
-positives = [52, 70] #tested positive before
-behaviors = [i for i in range(37,46)]+[i for i in range(55,69)] + [i for i in range(73,82)]# behaviors: wearing mask or shop indoors
-hunch = [49, 50, 51, 85, 86, 87]
-manual = states + covidlike + positives + behaviors + hunch
-setB = set(manual)
-
-setC = setB.union(setA)
-feat_idx = list(setC)
-print(feat_idx)
-
-
-'''
-
-
 #%% feature selection
 
 def select_feat(train_data, valid_data, test_data, select_all=True):
@@ -192,6 +147,7 @@ def trainer(train_loader, valid_loader, model, config, device):
                                 ,weight_decay=config['w_decay_rate']
                                  )
     writer = SummaryWriter() # Writer of tensoboard.
+    val_interval=10
 
     if not os.path.isdir('./models'):
         os.mkdir('./models') # Create directory of saving models.
@@ -235,7 +191,8 @@ def trainer(train_loader, valid_loader, model, config, device):
             loss_record.append(loss.item())
             
         mean_valid_loss = sum(loss_record)/len(loss_record)
-        print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
+        if (epoch + 1) % val_interval == 0:
+            print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
         # writer.add_scalar('Loss/valid', mean_valid_loss, step)
 
         if mean_valid_loss < best_loss:
@@ -257,7 +214,7 @@ config = {
     'seed': 520,      # Your seed number, you can pick your lucky number. :)
     'select_all': False,   # Whether to use all features.
     'valid_ratio': 0.25,   # validation_size = train_size * valid_ratio
-    'n_epochs': 5000,     # Number of epochs.            
+    'n_epochs': 10000,     # Number of epochs.            
     'batch_size': 256, 
     'w_decay_rate': 0.005, # weight regularization
     'learning_rate': 3e-4,              
@@ -313,10 +270,11 @@ def save_pred(preds, file):
 model = My_Model(input_dim=x_train.shape[1]).to(device)
 model.load_state_dict(torch.load(config['save_path']))
 preds = predict(test_loader, model, device) 
-save_pred(preds, 'pred.csv')      
+save_pred(preds, 'd11948002_hw1.csv')      
 
 #%%
 
-FileLink(r'pred.csv')
+100%10
+
 
 #%%
