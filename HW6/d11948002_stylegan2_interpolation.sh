0733bd359232becbe53202abbb747026a3a103ba
qqaazz800624 <qqaazz800624@gmail.com>
2023-04-04 18:09:04 +0800

    add bash file for stylegan2

---
+++ HW6/d11948002_stylegan2_interpolation.sh
@@ -0,0 +1,4 @@
+stylegan2_pytorch -- generate-interpolation \
+                  -- name d11948002_hw6_stylegan2\
+                  -- models_dir /home/u/qqaazz800624/2023_Machine_Learning/HW6/models/stylegan2 \
+                  -- interpolation-num-steps 5
---
+++ HW6/d11948002_stylegan2_train.sh
@@ -0,0 +1,8 @@
+stylegan2_pytorch --new \
+      --name d11948002_hw6_stylegan2 \
+      --data /neodata/ML/hw6_dataset/faces \
+      --num-train-steps 20000 \
+      --results_dir /home/u/qqaazz800624/2023_Machine_Learning/HW6/results/stylegan2 \
+      --models_dir /home/u/qqaazz800624/2023_Machine_Learning/HW6/models/stylegan2 \
+      --image-size 64 \
+      --network-capacity 32
