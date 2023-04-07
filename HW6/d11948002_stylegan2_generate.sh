aa69ba9c1d0b4385079c7f4d1fbfef78b2cf8227
qqaazz800624 <qqaazz800624@gmail.com>
2023-04-07 11:58:10 +0800

    delete the files

--- HW6/d11948002_stylegan2_generate.sh
+++
@@ -1,4 +0,0 @@
-stylegan2_pytorch --generate \
-                  --num_generate=10 \
-                  --image_size=64 \
-                  --num_image_tiles 1
--- HW6/d11948002_stylegan2_interpolation.sh
+++
@@ -1,7 +0,0 @@
-stylegan2_pytorch --generate --load-from {12}
-
-
-
-# stylegan2_pytorch --generate-interpolation \
-#                   --models_dir /home/u/qqaazz800624/2023_Machine_Learning/HW6/models/default \
-#                   --save-frames
