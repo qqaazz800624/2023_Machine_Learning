1ae19dce4a56f3a1810f94cc5e9c4ef702e9f203
qqaazz800624 <qqaazz800624@gmail.com>
2023-04-16 16:59:06 +0800

    update

--- HW6/d11948002_stylegan2_train.sh
+++ HW6/d11948002_stylegan2_train.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
 stylegan2_pytorch --data /neodata/ML/hw6_dataset/faces \
-                  --num-train-steps 60000 \
+                  --num-train-steps 35000 \
                   --image-size 64 
